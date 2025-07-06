981cfd2fc675c809a0935ae707879ce45052db79
lts-pj <lliamsymonds04@gmail.com>
2025-07-06 17:41:00 +1000

    added basic functionality for product

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -32,10 +32,57 @@ public class ProductController : ControllerBase
     }
 
     [HttpPost]
+
     public async Task<ActionResult<Products>> CreateProduct(Products product)
     {
         _context.Products.Add(product);
         await _context.SaveChangesAsync();
         return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateProduct(int id, Products product)
+    {
+        if (id != product.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(product).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!ProductExists(id))
+            {
+                return NotFound();
+            }
+            throw;
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    } 
+
+    private bool ProductExists(int id)
+    {
+        return _context.Products.Any(e => e.Id == id);
     }
 }
