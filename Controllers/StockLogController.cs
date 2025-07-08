b6b13a739e83ba5e8f1c12acdfc1f2f50dd6a911
lts-pj <lliamsymonds04@gmail.com>
2025-07-08 18:54:12 +1000

    added stock log controller

---
+++ Controllers/StockLogController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using InventoryAPI.Models;
+using InventoryAPI.Services;
+
+namespace InventoryAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StockLogController : ControllerBase
+{
+    private readonly StockLogService _stockLogService;
+
+    public StockLogController(StockLogService stockLogService)
+    {
+        _stockLogService = stockLogService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<StockLog>>> GetStockLogs(
+        [FromQuery] int? productId = null,
+        [FromQuery] int? warehouseId = null,
+        [FromQuery] DateTime? fromDate = null,
+        [FromQuery] DateTime? toDate = null,
+        [FromQuery] string? changeType = null
+    )
+    {
+        var logs = await _stockLogService.GetStockLogsAsync(productId, warehouseId, fromDate, toDate, changeType);
+        return Ok(logs);
+    }
+}
