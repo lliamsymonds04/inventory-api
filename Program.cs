511ddb27492f4110af516ee7c6883172c9323cfe
lts-pj <lliamsymonds04@gmail.com>
2025-07-11 11:33:13 +1000

    configured user secrets

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using InventoryAPI.Data;
 using InventoryAPI.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,19 +10,39 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
+builder.Services.AddAuthorization();
 
-//register custom services
+// Build connection string
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var databasePassword = builder.Configuration["DatabasePassword"];
+
+if (string.IsNullOrEmpty(databasePassword))
+{
+    throw new InvalidOperationException("Database password is not configured.");
+}
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("Database connection string is not configured.");
+}
+
+connectionString = connectionString.Replace("{DatabasePassword}", databasePassword);
+
+// Add DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register custom services
 builder.Services.AddScoped<StockLogService>();
 
+// Configure JWT authentication
 var jwtSettings = builder.Configuration.GetSection("JwtSettings");
-var secretKey = jwtSettings["SecretKey"];
-
-if (string.IsNullOrEmpty(secretKey))
+var jwtSecretKey = jwtSettings["SecretKey"];
+if (string.IsNullOrEmpty(jwtSecretKey))
 {
     throw new InvalidOperationException("JWT Secret Key is not configured.");
 }
 
-// Configure JWT authentication
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -37,11 +58,9 @@ builder.Services.AddAuthentication(options =>
         ValidateIssuerSigningKey = true,
         ValidIssuer = jwtSettings["Issuer"],
         ValidAudience = jwtSettings["Audience"],
-        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(secretKey))
+        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(jwtSecretKey))
     };
 });
-
-builder.Services.AddAuthorization();
 
 // Add CORS policy
 builder.Services.AddCors(options =>
@@ -53,11 +72,7 @@ builder.Services.AddCors(options =>
                           .AllowCredentials());
 });
 
-// Add database context
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
+// Build App
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -65,10 +80,16 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+// Exception handling
+app.UseMiddleware<GlobalExceptionMiddleware>();
+
+// Cors
+app.UseCors("AllowFrontend");
+
+app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
-app.UseCors("AllowFrontend");
-app.UseMiddleware<GlobalExceptionMiddleware>();
 app.Run();
