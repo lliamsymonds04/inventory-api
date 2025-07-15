f5e1cf03dbb85118d15496fc11d8fb4bcb512be8
lts-pj <lliamsymonds04@gmail.com>
2025-07-15 17:59:06 +1000

    added jwt cookie middleware

---
+++ Middleware/JwtFromCookie.cs
@@ -0,0 +1,27 @@
+public class JwtFromCookie
+{
+    private readonly RequestDelegate _next;
+    private readonly IConfiguration _config;
+
+    public JwtFromCookie(RequestDelegate next, IConfiguration configuration)
+    {
+        _next = next;
+        _config = configuration;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        // Get the cookie name
+        var jwtCookieName = _config["JwtSettings:CookieName"] ?? "jwt";
+
+        // Check if the JWT cookie exists
+        if (context.Request.Cookies.TryGetValue(jwtCookieName, out var jwt))
+        {
+            // Set the JWT in the Authorization header
+            context.Request.Headers["Authorization"] = $"Bearer {jwt}";
+        }
+
+        // Call the next middleware in the pipeline
+        await _next(context);
+    }
+}
