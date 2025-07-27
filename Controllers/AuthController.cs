186ed4332db9744e62829d65df6b1af37795100c
Lliam <lliamsymonds04@gmail.com>
2025-07-27 22:14:06 +1000

    set same site mode to none for auth cookie

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -97,7 +97,7 @@ public class AuthController : ControllerBase
         {
             HttpOnly = true,
             Secure = true,
-            SameSite = SameSiteMode.Strict,
+            SameSite = SameSiteMode.None,
             Expires = DateTime.UtcNow.AddDays(-1) // Set to a past date to clear the cookie
         };
 
